92fc5c69208380767a96e643a6fe8b08ae47eb86
Cidonzera <111708625+Cidonzera@users.noreply.github.com>
2025-11-25 23:31:13 -0300

    Update firebase-config.js

--- firebase-config.js
+++ firebase-config.js
@@ -1,3 +1,4 @@
+// Configuração Firebase para uso global no navegador
 window.firebaseConfig = {
   apiKey: "AIzaSyCq9ItRenohm9tl9Aol59gyRz81XOD3msc",
   authDomain: "projeto-enem-e3451.firebaseapp.com",
@@ -7,5 +8,8 @@ window.firebaseConfig = {
   appId: "1:97481254094:web:eea0f85ebed6838cfcdc9f"
 };
 
+// Token customizado opcional (normalmente fica undefined)
 window.initialAuthToken = undefined;
+
+// ID usado nas coleções
 window.appId = 'prep-enem-default-app-id';
