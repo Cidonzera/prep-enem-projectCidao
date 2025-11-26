2a708e8fbed1f7a8556b81931d292cfafa849b42
Cidonzera <111708625+Cidonzera@users.noreply.github.com>
2025-11-25 23:13:07 -0300

    Update Firebase configuration details

--- firebase-config.js
+++ firebase-config.js
@@ -1,10 +1,10 @@
 export const firebaseConfig = {
-  apiKey: "AIzaSyD0sGz...",
-  authDomain: "meu-projeto.firebaseapp.com",
-  projectId: "meu-projeto",
-  storageBucket: "meu-projeto.appspot.com",
-  messagingSenderId: "123456789",
-  appId: "1:123456789:web:abcdef123456"
+  apiKey: "AIzaSyCq9ItRenohm9tl9Aol59gyRz81XOD3msc",
+  authDomain: "projeto-enem-e3451.firebaseapp.com",
+  projectId: "projeto-enem-e3451",
+  storageBucket: "projeto-enem-e3451.firebasestorage.app",
+  messagingSenderId: "97481254094",
+  appId: "1:97481254094:web:eea0f85ebed6838cfcdc9f"
 };
 export const initialAuthToken = undefined;
 export const appId = 'prep-enem-default-app-id';
